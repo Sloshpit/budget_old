ae45d69ef0e5c693ffee62b121735794c8209588
Sloshpit <neel.maheshwari@gmail.com>
2020-04-23 19:35:05 -0400

    Fix base on categories
    The base is now fixed.  The category app was inhereting base.html from transactions.  Therefore if running different apps and you want different bases for whatever reason, you need to have different filenames for the base.html.  Or change this to have a master base file if it can function on all apps.

--- categories/views.py
+++ categories/views.py
@@ -4,15 +4,11 @@ from django.db.models import Sum
 from transactions.models import Transaction
 from .models import Category
 
-#def index(request):
-#    show_categories = Category.objects.all()
-#   output = ', '.join[c.category for c in show_categories]
-#    return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
+    template = loader.get_template('categories/index.html')
     show_categories = Category.objects.all()
     show_transactions = Transaction.objects.all()
-    template = loader.get_template('categories/index.html')
     total = Transaction.objects.all().aggregate(sum=Sum('amount'))['sum'] or 0.00
     total = "{:.2f}".format(total)
 
